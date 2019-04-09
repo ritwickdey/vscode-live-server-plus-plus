0c4791bd78dff1821d0321b8e920b9dc305e4955
Ritwick Dey <ritwickdey@outlook.com>
2019-04-09 18:11:58 +0530

    websocket

--- src/core/LiveServerPlusPlus.ts
+++ src/core/LiveServerPlusPlus.ts
@@ -1,28 +1,74 @@
+import * as vscode from 'vscode';
 import * as http from 'http';
+import * as WebSocket from 'ws';
 import { IncomingMessage, ServerResponse } from 'http';
 import * as path from 'path';
 import { WorkspaceUtils } from './WorkSpaceUtils';
 import { readFile } from './FileSystem';
+import { URL } from 'url';
 
 export class LiveServerPlusPlus {
   workspace: WorkspaceUtils;
+  server: http.Server;
+  ws: WebSocket.Server;
 
   constructor() {
+    this.server = http.createServer(this.routesHandler);
+    this.ws = new WebSocket.Server({ noServer: true });
     this.workspace = new WorkspaceUtils();
     this.routesHandler = this.routesHandler.bind(this);
   }
 
   async goLive() {
-    await this.createServer();
+    await this.listenServer();
     console.log('Server is created');
+    vscode.workspace.onDidChangeTextDocument(event => {
+      this.broadcastWs({
+        fileName: event.document.fileName,
+        text: event.document.getText()
+      });
+    });
   }
 
-  private createServer() {
+  private listenServer() {
     return new Promise(resolve => {
-      const server = http.createServer(this.routesHandler);
-      server.listen(9000, () => {
+      this.listenWs();
+      this.server.listen(9000, () => {
         resolve();
       });
+    });
+  }
+
+  private broadcastWs(data: any, action = 'reload') {
+    this.ws.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(
+          JSON.stringify({
+            data,
+            action
+          })
+        );
+      }
+    });
+  }
+
+  private listenWs() {
+    this.ws.on('connection', ws => {
+      ws.send('Connected!');
+    });
+
+    this.ws.on('close', () => {
+      console.log('disconnected');
+    });
+
+    this.server.on('upgrade', (request, socket, head) => {
+      if (request.url === '/_ws') {
+        this.ws.handleUpgrade(request, socket, head, ws => {
+          this.ws.emit('connection', ws, request);
+        });
+      } else {
+        socket.destroy();
+      }
     });
   }
 
